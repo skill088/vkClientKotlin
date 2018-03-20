2c59aca9b82fd3a743a0e03c59487ebe19a1a208
Vladimir Kotov <skill088@mail.ru>
2018-03-20 15:30:28 +0300

    First commit

---
+++ app/src/main/java/com/projects/vo1/customvk/AuthorizationActivity.kt
@@ -0,0 +1,12 @@
+package com.projects.vo1.customvk
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthorizationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization)
+    }
+}
